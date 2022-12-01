23ea30af8e196970f3ae442335525c42e539df32
VyacheslavGeraskin <geraskin707@gmail.com>
2022-12-01 20:46:56 +0300

    Fix config file

--- src/app/services/api/auth.service.ts
+++ src/app/services/api/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {map, Observable, tap} from "rxjs";
+import {Observable, tap} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {JwtHelperService} from "@auth0/angular-jwt";
 
